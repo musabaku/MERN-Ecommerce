26ce9ceb741751207829a4be5e1bee806f19b4a7
MUSAB AHMED KHAN UMAIR <110651555+musabaku@users.noreply.github.com>
2023-06-03 19:31:37 +0300

    Update productController.js

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModels");
 
+// Create product
+
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
@@ -7,23 +9,65 @@ exports.createProduct = async (req, res, next) => {
     product,
   });
 };
+// update product
 
 exports.updateProduct = async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
-    if (!product){
-        return res.status(500).json({
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
 
-        
-        })
-
-    }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: true,
+  });
 
   res.status(201).json({
     success: true,
     product,
   });
 };
+// delete product
+
+exports.deleteProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Product deleted",
+  });
+};
+// get one product
+
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+// get all product
 
 exports.getAllProducts = async (req, res, next) => {
   const product = await Product.find();
