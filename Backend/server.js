93ba06d0ff96ef9522a14555138ac86a249ad796
MUSAB AHMED KHAN UMAIR <110651555+musabaku@users.noreply.github.com>
2023-06-03 21:18:18 +0300

    handling uncaught exception, unhandled promise-rejection

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,29 @@
-const app = require("./app")
+const app = require("./app");
 
-const dotenv = require("dotenv")
-const connectDatabase = require("./database")
-dotenv.config({path:"Backend/config/config.env"})
+const dotenv = require("dotenv");
+const connectDatabase = require("./database");
+dotenv.config({ path: "Backend/config/config.env" });
 
-connectDatabase()
+// handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error message : ${err.message}`);
+  console.log("Closing Server due to uncaught exception");
+  process.exit(1);
+});
 
+connectDatabase();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is working on http://localhost:${process.env.PORT}`)
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// Unhandled promise rejection
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error message : ${err.message}`);
+  console.log("Closing Server due to Unhandled promise rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
