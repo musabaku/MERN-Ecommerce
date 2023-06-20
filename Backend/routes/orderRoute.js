c620edb5e04dbfd99df8f029a8c60672dff38fad
MUSAB AHMED KHAN UMAIR <110651555+musabaku@users.noreply.github.com>
2023-06-20 22:41:15 +0300

    Add files via upload

---
+++ Backend/routes/orderRoute.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController")
+const { isAuthenticatedUser, authorizeRole } = require("../middleware/auth");
+
+const router = express.Router()
+
+
+router.route("/order/new").post(isAuthenticatedUser,newOrder)
+router.route("/order/:id").get(isAuthenticatedUser,authorizeRole("admin"),getSingleOrder)
+router.route("/order/me").post(isAuthenticatedUser,myOrders)
+
+
+module.exports= router
